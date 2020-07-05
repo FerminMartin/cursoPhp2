653cbe3f6d86e902488905d671f5488a7d665750
Fermin Martin <fermartorr01@gmail.com>
2020-07-06 00:01:43 +0200

    Organizamos nuestros archivos en carpetas  MVC (Modelos, Vistas y Controladores)2 de 3

--- public/index.php
+++ public/index.php
@@ -37,15 +37,46 @@ $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
 
 $routerContainer = new RouterContainer();
 $map = $routerContainer->getMap();
-$map->get('index', '/cursophp2/', '../index.php');
-$map->get('addJobs', '/cursophp2/jobs/add', '../addJob.php');
+$map->get('index', '/cursophp2/', [
+    'controller' => 'App\Controllers\IndexController',
+    'action' => 'indexAction'
+]);
+$map->get('addJobs', '/cursophp2/jobs/add',[
+    'controller' => 'App\Controllers\JobsController',
+    'action' => 'getAddJobAction'
+] );
 
 $matcher = $routerContainer->getMatcher();
 $route = $matcher->match($request);
+     
+function printElement($job){
+    // if($job->visible == false) {
+    //     return;             
+    // }
+      
+        echo '<li class="work-position">';
+        echo '<h5>' .  $job->title . '</h5>';
+        echo '<p>' .  $job->description . '</p>';
+        echo '<p>' .  $job ->getDurationAsString() . '</p>';
+        echo '<strong>Achievements:</strong>';
+        echo '<ul>';
+        echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+        echo ' <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+        echo '  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+        echo '</ul>';
+        echo '</li>';
+      }
+
 if (!$route){
     echo 'No route';
 } else {
-    require $route->handler;
+    // var_dump($route->handler);
+    $handlerData = $route->handler;
+    $controllerName = $handlerData['controller'];
+    $actionName = $handlerData['action'];
+
+    $controller = new $controllerName;
+    $controller->$actionName();
 }
 
 
