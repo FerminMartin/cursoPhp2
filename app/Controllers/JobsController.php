7a4e7cd9c0a8702288ddbefbd2c9dc9dce4d672d
Fermin Martin <fermartorr01@gmail.com>
2020-07-09 16:35:31 +0200

    Trabajamos las validaciones y la subida de archivos (GUARDAMOS EL ARCHIVO EN LA BASE EN LA BD PERO NO LO IMPRIMIME EN INDEX.TWIG AUNQUE SI LO RECONOCE)

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -12,14 +12,26 @@ class JobsController extends BaseController{
             $postData = $request->getParsedBody();
             $jobValidator = v::key('title', v::stringType()->notEmpty())
             ->key('description', v::stringType()->notEmpty());
+            // ->key('logo', v::stringType()->notEmpty());
 
 
             try{
                 $jobValidator->assert($postData);
                 $postData = $request->getParsedBody();
+
+                $files = $request->getUploadedFiles();
+                $logo = $files['logo'];
+
+                if($logo->getError() == UPLOAD_ERR_OK){
+                    $fileName = $logo->getClientFilename();
+                    $logo->moveTo("uploads/$fileName");
+                }
+
                 $job = new Job();
                 $job->title = $postData['title'];
                 $job->description = $postData['description'];
+                $job->visible = true;
+                $job->fileName = "uploads/$fileName";
                 $job->save();
 
                 $responseMessage = 'Saved';
